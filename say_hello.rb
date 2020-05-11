c30848b1cb73b918a4088361322f2766252fc066
Betzalel Levi <betzalelleviva@gmail.com>
2020-05-11 19:19:55 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,4 @@
 def say_hello(name = "Ruby Programmer")
 puts "Hello #{name}!"
 end
-# name = "Betzalel"
-# puts 1
-# say_hello("name")
-# puts 2
-# say_hello(name)
-# puts 3
-# say_hello
-# say_hello("Betzalel")
-# hi
+say_hello("Betzalel")
