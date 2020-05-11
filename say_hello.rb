6c7bfcc61f0c324738a4a2cb68c0614b6e3043b8
Betzalel Levi <betzalelleviva@gmail.com>
2020-05-11 19:45:51 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,4 @@
 Betzalel = ["Betzalel", 27, "Caregiver"]
 puts "#{Betzalel[0]}"
-puts " Name: Betzalel[0], Age: Betzalel[1], Occupation: [2]"
+puts " Name: Betzalel[0], Age: Betzalel[1], Occupation:[2]"
+
